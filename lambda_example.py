38e27babb135f50d1a77f6db3252bae2a6775b24
Leon Lei <leonlei35t@gmail.com>
2017-08-11 20:06:40 -0400

    added lambda_example, mySQLConnector, tkinter, urllib

---
+++ lambda_example.py
@@ -0,0 +1,11 @@
+people = ['Dr. Ben Carver', 'Mr. Bill Sash', 'Mrs. Wendy Marvel', 'Mr. Mas Lill']
+
+def split_title_last(person):
+    return person.split()[0] + ' ' + person.split()[-1]
+
+# option 1
+for person in people:
+    print(split_title_last(person) == (lambda x: x.split()[0] + ' ' + x.split()[-1])(person))
+
+# option 2
+print(list(map(split_title_last, people,)) == list(map(lambda person: person.split()[0] + ' ' + person.split()[-1], people)))
