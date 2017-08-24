7f720b355a6d86137b88ecdfc9afe0e755742dea
Leon Lei <leonlei35t@gmail.com>
2017-08-23 22:47:19 -0400

    arg_kwargs.py and enumerate_practice.py modified. SQL command txt file and test_pymysql.py added

--- enumerate_practice.py
+++ enumerate_practice.py
@@ -1,28 +1,32 @@
-example = ['left', 'right', 'up', 'down']
+def main():
+    example = ['left', 'right', 'up', 'down']
 
-# for i in range(len(example)):
-#     print(i, example[i])
+    # for i in range(len(example)):
+    #     print(i, example[i])
 
-# cleaner way to print out both index and value
-for i,j in enumerate(example):
-    print(i, j)
+    # cleaner way to print out both index and value
+    for i,j in enumerate(example):
+        print(i, j)
 
-# create a dictionary using enumerate off the list
-new_dict = dict(enumerate(example))
-print(new_dict)
+    # create a dictionary using enumerate off the list
+    new_dict = dict(enumerate(example))
+    print(new_dict)
 
-# printing the k,v is a little weird only because our keys are also the count
-[print(i, j) for i, j in enumerate(new_dict.items())]
+    # printing the k,v is a little weird only because our keys are also the count
+    [print(i, j) for i, j in enumerate(new_dict.items())]
 
-# printing the k,v from this dict is easier to comphrehend
-example_dict = {'left':'<',
-                'right':'>',
-                'up':'^',
-                'down':'v',}
+    # printing the k,v from this dict is easier to comphrehend
+    example_dict = {'left':'<',
+                    'right':'>',
+                    'up':'^',
+                    'down':'v',}
 
-[print(i, j) for i, j in enumerate(example_dict.items())]
+    [print(i, j) for i, j in enumerate(example_dict.items())]
 
-# 0 ('left', '<')
-# 1 ('right', '>')
-# 2 ('up', '^')
-# 3 ('down', 'v')
+    # 0 ('left', '<')
+    # 1 ('right', '>')
+    # 2 ('up', '^')
+    # 3 ('down', 'v')
+
+if __name__ == '__main__':
+    main()
---
+++ test_pymysql.py
@@ -0,0 +1,41 @@
+import pymysql
+
+# host/server, user, password, database
+conn = pymysql.connect('localhost', 'root', '3510', 'beginnerwork')
+cur = conn.cursor()
+
+# using variables for inserting into table
+timea = 15856605
+username = 'pythonista'
+tweet = "live from new york, it's SNL"
+
+cur.execute("INSERT INTO ham (time, username, tweet) VALUES (%s,%s,%s)",
+                (timea, username, tweet))
+
+cur.execute("""
+    CREATE TABLE menu (
+        ID INT NOT NULL AUTO_INCREMENT,
+        Food VARCHAR(255) NOT NULL,
+        Price INT(5) NOT NULL,
+        PRIMARY KEY (ID)
+    )
+""")
+
+conn.commit()
+
+# 2 ways to input the SQL commands
+# sql = 'SELECT * FROM ham'
+# cur.execute(sql)
+cur.execute("SELECT * FROM ham")
+
+# 2 ways to print out the query
+# for row in cursor:
+#     print(row)
+rows = cur.fetchall()
+for eachRow in rows:
+    print(eachRow)
+
+# always close the database connection
+conn.close()
+
+print('\nProgram Complete')
