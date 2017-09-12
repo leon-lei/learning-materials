9b04c77ca8658f3c347239850086e6055cbd0fe8
Leon Lei <leonlei35t@gmail.com>
2017-09-11 21:40:12 -0400

    numpy tutorials added. pandas tutorial edited

--- pandas_tutorials/pandas_practice7_pct_change.py
+++ pandas_tutorials/pandas_practice7_pct_change.py
@@ -19,6 +19,7 @@ def grab_initial_state_data():
         query = 'FMAC/HPI_' + str(abbv)
         df = quandl.get(query, authtoken=api_key)
         df.rename(columns={'Value':str(abbv)}, inplace=True)
+        df[abbv] = (df[abbv] - df[abbv][0]) / df[abbv][0] * 100.0
 
         if main_df.empty:
             main_df = df
@@ -30,23 +31,33 @@ def grab_initial_state_data():
     # pickling is similar to creating and writing onto a csv
     # pickle_out is the new file, in byte code
     # pickle.dump is the writer
-    pickle_out = open('fiddy_states.pickle', 'wb')
+    pickle_out = open('fiddy_states_pct.pickle', 'wb')
     pickle.dump(main_df, pickle_out)
     pickle_out.close()
 
-grab_initial_state_data()
+def HPI_Benchmark():
+    df = quandl.get('FMAC/HPI_USA', authtoken=api_key)
+    df.rename(columns={'Value': 'United States'}, inplace=True)
+    df['United States'] = (df['United States'] - df['United States'][0]) / df['United States'][0] * 100.0
+    return df
 
-# pickling using og python module
-pickle_in = open('fiddy_states.pickle', 'rb')
-HPI_data = pickle.load(pickle_in)
-# print(HPI_data)
+# grab_initial_state_data()
 
-# pickling using panda module
-# HPI_data.to_pickle('pandas_pickle.pickle')
-# HPI_data2 = pd.read_pickle('pandas_pickle.pickle')
-# print(HPI_data2)
+fig = plt.figure()
+ax1 = plt.subplot2grid((1,1),(0,0))
 
 # plot the data frame
-HPI_data.plot()
+HPI_data = pd.read_pickle('fiddy_states_pct.pickle')
+benchmark = HPI_Benchmark()
+
+HPI_data.plot(ax=ax1)
+benchmark.plot(ax=ax1, color='k', linewidth=10)
+
 plt.legend().remove()
 plt.show()
+
+# shows how each state correlates with each other
+# describe method to see count, mean, std, min/max, percentiles
+HPI_State_Correlation = HPI_data.corr()
+print(HPI_State_Correlation)
+print(HPI_State_Correlation.describe())
