8b58ad9a761d795ed91079aa4829d322feb742d4
Leon Lei <leonlei35t@gmail.com>
2017-09-12 23:02:17 -0400

    Adjusted bs4 modules. Combined git commands.txt

--- beautiful_soup_tutorials/beautifulsoup_pt3_tables_xml.py
+++ beautiful_soup_tutorials/beautifulsoup_pt3_tables_xml.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import bs4 as bs
 import urllib.request
 import pandas as pd
@@ -26,33 +25,4 @@ for df in dfs:
 sauce = urllib.request.urlopen('https://pythonprogramming.net/sitemap.xml').read()
 stew = bs.BeautifulSoup(sauce, 'xml')
 for url in stew.find_all('loc'):
-=======
-import bs4 as bs
-import urllib.request
-import pandas as pd
-
-source = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
-soup = bs.BeautifulSoup(source, 'lxml')
-
-table = soup.table
-# table = soup.find('table')
-# print(table)
-
-table_rows = table.find_all('tr')
-
-for tr in table_rows:
-    td = tr.find_all('td')
-    row = [i.text for i in td]
-    print(row)
-
-# using pandas to illustrate same task
-dfs = pd.read_html('https://pythonprogramming.net/parsememcparseface/', header=0)
-# print(type(dfs))  # class 'list'
-for df in dfs:
-    print(df)
-
-sauce = urllib.request.urlopen('https://pythonprogramming.net/sitemap.xml').read()
-stew = bs.BeautifulSoup(sauce, 'xml')
-for url in stew.find_all('loc'):
->>>>>>> origin/master
     print(url.text)
