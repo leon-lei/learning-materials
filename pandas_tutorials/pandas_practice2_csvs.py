5f2646e44cf4c9a282f8dd71440931e096bb8112
Leon Lei <leonlei35t@gmail.com>
2017-09-14 10:57:14 -0400

    Cleaned up redundant code

--- pandas_tutorials/pandas_practice2_csvs.py
+++ pandas_tutorials/pandas_practice2_csvs.py
@@ -7,7 +7,6 @@ from matplotlib import style
 # importing from csv and outputting to csv
 df = pd.read_csv('ZILLOW-Z77002_ZRISFRR.csv')
 df.set_index('Date', inplace=True)
-print(df.head())
 df.to_csv('newcsv2.csv')
 
 # set the index column when opening a csv
@@ -23,11 +22,6 @@ df.rename(columns={'Houston_SFR':'Houston_family'})
 # read a csv with no headers and assign headers and index_col
 df.to_csv('newcsv3.csv', header=False)
 df = pd.read_csv('newcsv3.csv', names=['Date','Houston_SFR'], index_col=0)
-
-# rename column in data frame
-df.rename(columns={'Houston_SFR':'Houston_family'}, inplace=True)
-
-print(df.head())
 
 # save data frame to html
 df.to_html('csv_file.html')
