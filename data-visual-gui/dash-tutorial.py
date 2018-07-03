86bdae4d4d0e2b96545f47bdc8032dfde243309d
Leon Lei <leonlei35t@gmail.com>
2018-07-03 00:58:14 -0400

    Enhancement: tutorial takes user input for output

--- data-visual-gui/dash-tutorial.py
+++ data-visual-gui/dash-tutorial.py
@@ -1,23 +1,22 @@
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
 app = dash.Dash()
 
 app.layout = html.Div(children=[
-    html.H1('Dash tutorials'),
-    dcc.Graph(id='example',
-            figure={
-                'data': [
-                    {'x':[1,2,3,4,5], 'y':[12,4,15,18,7], 'type':'line', 'name':'boats'},
-                    {'x':[1,2,3,4,5], 'y':[5,17,4,12,15], 'type':'bar', 'name':'cars'},
-                    ],
-                'layout': {
-                    'title':'Basic Dash Example'
-                }
-            })
+   dcc.Input(id='input', value='Enter something', type='text'),
+   html.Div(id='output')
     ])
 
-
+@app.callback(
+    Output(component_id='output', component_property='children'),
+    [Input(component_id='input', component_property='value')])
+def update_value(input_data):
+    try:
+        return str(float(input_data)**2)
+    except:
+        return "Some error"
 if __name__=='__main__':
     app.run_server(debug=True)
