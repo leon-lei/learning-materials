f9a62f55821804a3d2691fb20fb08d594d2055fe
Leon Lei <leonlei35t@gmail.com>
2018-10-20 23:53:50 -0400

    Update: another example in type_annot

--- type_annot.py
+++ type_annot.py
@@ -3,5 +3,17 @@ def func(a: int, b: int) -> int:
     """
     return a + b
 
+
+def func2(a: list, b: str) -> str:
+
+    for x in a:
+        print(f'{b} {x}')
+
+    return 'Success'
+
+
 if __name__ == '__main__':
     print(func(2,5))
+
+    names = ['alpha', 'beta', 'gamma']
+    print(func2(names, 'hello'))
