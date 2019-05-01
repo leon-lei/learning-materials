717dec61b05b4b9372fa666fa63fbe5c49432904
Leon Lei <leonlei35t@gmail.com>
2019-05-01 00:58:05 -0400

    Update: multiprocessing real python example

--- advance/multiprocessing_practice.py
+++ advance/multiprocessing_practice.py
@@ -1,24 +1,40 @@
+# Original code from Dan Bader Real Python Youtube channel
+# Modified namedtuple parameters
+import collections
 import multiprocessing
-from multiprocessing import Pool
+import os
+import time
+from pprint import pprint
 
-# multiprocessing without returning any data
-def spawn(num,num2):
-    print('Spawned! {} {}'.format(num,num2))
+Scientist = collections.namedtuple('Scientist', [
+    'name',
+    'born',
+])
 
-if __name__ == '__main__':
-    for i in range(100):
-        p = multiprocessing.Process(target=spawn, args=(i,i+1))
-        p.start()
-        p.join()
+scientists = (
+    Scientist(name='alpha', born=1991),
+    Scientist(name='beta', born=1978),
+    Scientist(name='gamma', born=1998),
+    Scientist(name='delta', born=2013),
+    Scientist(name='epsilon', born=1987),
+)
 
-# multiprocessing returnin data
-def job(num):
-    return num*2
+print(scientists, '\n')
 
-if __name__ == '__main__':
-    p = Pool(processes=20)
-    data = p.map(job, range(200))
-    data2 = p.map(job, [5,2])
-    p.close()
-    print(type(data))
-    print(type(data2))
+def transform(x):
+    print(f'Processing {os.getpid()} working record {x.name}')
+    time.sleep(2)
+    result = {'name': x.name, 'age': 2019 - x.born}
+    print(f'Process {os.getpid()} done processing record {x.name}')
+    return result
+
+start = time.time()
+
+pool = multiprocessing.Pool()    # spawns as many processes as machine's cpu cores
+# pool = multiprocessing.Pool(processes=2)    # specifying set number of processes
+# pool = multiprocessing.Pool(processes=len(scientists))    # dynamic
+result =  pool.map(transform, scientists)
+
+end = time.time()
+print(f'\nTime to complete: {end - start:.2f}s\n')
+pprint(result)
