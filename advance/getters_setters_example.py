f8afa9c70130ffcc9d09627316c680aeb6bbd0d0
Leon Lei <leonlei35t@gmail.com>
2019-05-13 22:50:00 -0400

    Update: getter_setter example

--- advance/getters_setters_example.py
+++ advance/getters_setters_example.py
@@ -30,3 +30,23 @@ print(foo.__dict__)
 
 foo.protected_value = 200 # raises ValueError
 del foo.protected_value # raises AttributeError
+
+
+# Another example from Python Cookbook
+class Person:
+    def __init__(self, first_name):
+        self.first_name = first_name
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, value):
+        if not isinstance(value, str):
+            raise TypeError('Expect a string')
+        self._first_name = value
+
+    @first_name.deleter
+    def first_name(self):
+        raise AttributeError("Can't delete attribute")
